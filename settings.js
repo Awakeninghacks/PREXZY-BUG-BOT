8b1801eb46bcec550c142b85b88ec1a7ebfb1a07
Awakeninghacks <166938169+Awakeninghacks@users.noreply.github.com>
2024-09-16 23:11:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '639537644957'  //creator number
+global.ownername = 'G3TFckD' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
